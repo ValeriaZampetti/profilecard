a47c41edb9347726882256a0e5e0a8bc6ef9abe0
ValeriaZampetti <vzampetti@correo.unimet.edu.ve>
2022-05-12 23:32:28 +0000

    ejercicio

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,4 @@
+import { name } from "file-loader";
 import "../style/index.css";
 
 /**
@@ -33,14 +34,14 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
+          <h1>${variables.name} ${variables.lastname}</h1>
+          <h2>${variables.role} </h2>
+          <h3>${variables.country} ${variables.city}</h3>
           <ul class="position-right">
             <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+            <li><a href=ValeriaZampetti:master><i class="fab fa-github"></i></a></li>
+            <li><a href=https://www.linkedin.com/feed/><i class="fab fa-linkedin"></i></a></li>
+            <li><a href=https://www.instagram.com><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
@@ -54,7 +55,8 @@ window.onload = function() {
     // if includeCover is true the algorithm should
     includeCover: true,
     // this is the url of the image that will used as background for the profile cover
-    background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da",
+    background:
+      "https://trucoslondres.com/wp-content/uploads/2017/05/playa.jpg",
     // this is the url for the profile avatar
     avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
     // social media bar position (left or right)
