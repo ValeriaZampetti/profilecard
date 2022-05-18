2dce29d22f78bcba1c7c2192b5a2ed77df22fe94
ValeriaZampetti <vzampetti@correo.unimet.edu.ve>
2022-05-18 02:29:17 +0000

    listo card

--- src/js/app.js
+++ src/js/app.js
@@ -37,11 +37,19 @@ function render(variables = {}) {
           <h1>${variables.name} ${variables.lastname}</h1>
           <h2>${variables.role} </h2>
           <h3>${variables.country} ${variables.city}</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href=ValeriaZampetti:master><i class="fab fa-github"></i></a></li>
-            <li><a href=https://www.linkedin.com/feed/><i class="fab fa-linkedin"></i></a></li>
-            <li><a href=https://www.instagram.com><i class="fab fa-instagram"></i></a></li>
+          <ul class="${variables.socialMediaPosition}">
+            <li><a href="https://twitter.com/${
+              variables.twitter ? variables.twitter : ""
+            }"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://github.com/${
+              variables.github ? variables.github : ""
+            }"><i class="fab fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/${
+              variables.linkedin ? variables.linkedin : ""
+            }"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/${
+              variables.instagram ? variables.instagram : ""
+            }"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
